6cb6c44f093929e6db40628273f7477ffaf31018
is.derkach <is.derkach@severx.ru>
2023-03-03 05:00:35 +0600

    cleanup commit

--- utils/detector.py
+++ utils/detector.py
@@ -1,30 +1,33 @@
-import os
-import sys
+import cv2 as cv
+from os import path as os
+from sys import argv
 from logger import logger
 from json import loads
 
+### Дефолтные значения
+PARAMS={
+    "MIN_MATCHES":10,
+    "FLN_INDEX":{"algorithm":1, "trees":5},
+    "FLN_SEARCH":{"checks":50},
+    "HOMOGRAPHY":{"method":cv.RANSAC, "ransacReprojThreshold":5.0}
+}
 
 @logger
 def detector(
-    query_img_path:str,
-    train_img_path:str,
-    params:dict={
-        ### Дефолтные значения
-        "MIN_MATCH_COUNT":10,
-        "FLANN_INDEX_PARAMS":{"algorithm":1, "trees":5},
-        "FLANN_SEARCH_PARAMS":{"checks":50}
-    },
-    log_to_file:bool=False,
+    query_path:str,
+    train_path:str,
+    params:dict=PARAMS,
+    logfile:bool=True,
     log:object=None,
     attempt:int=1,
     **kwargs
 ) -> None:
     """ Определение объекта на изображении
 
-    >>> ... query_img_path ~ (str) - путь к фрагменту
-    >>> ... train_img_path ~ (str) - путь к изображению
+    >>> ... query_path ~ (str) - путь к фрагменту
+    >>> ... train_path ~ (str) - путь к изображению
     >>> ... params ~ (dict) - набор корректирующих параметров
-    >>> ... log_to_file ~ (bool) - способ логирования процесса выполнения:
+    >>> ... logfile ~ (bool) - способ логирования процесса выполнения:
                 = True - в файл директории logs/
                 = False - в sys.stdout
     >>> ... log ~ (object) – объект, реализующий логирование
@@ -32,43 +35,46 @@ def detector(
     >>> return (None) – функция логирует результат в sys.stdout
     """
 
-    import cv2 as cv
     import numpy as np
     from math import acos, sqrt, pi
     from matplotlib import pyplot as plt
 
-    min_match_count, flann_index_params, flann_search_params = params
+    global PARAMS
+
+    MIN_MATCHES, FLN_INDEX, FLN_SEARCH, HOMOGRAPHY = {**PARAMS, **params}.values()
     log.info('Параметры детекции:\n"{}"', params)
 
-    query_img = cv.imread(log.transfer(query_img_path, 'query'), cv.IMREAD_GRAYSCALE) ### объект
-    train_img = cv.imread(log.transfer(train_img_path, 'train'), cv.IMREAD_GRAYSCALE) ### изображение
+    log.info('Обработка изображений:')
+    query_img = cv.imread(log.transfer(query_path, 'query'), cv.IMREAD_GRAYSCALE) ### фрагмент
+    train_img = cv.imread(log.transfer(train_path, 'train'), cv.IMREAD_GRAYSCALE) ### изображение
     log.info('Изображения прочитаны')
     
-    sys.exit()
-
-    sift = cv.SIFT_create() ### инициализация детектора
+    detector = cv.SIFT_create() ### инициализация детектора
+    log.info('Детектор инициализирован: {}', detector.__class__)
     ### Поиск ключевых точек и дескрипторов
-    query_key, query_des = sift.detectAndCompute(query_img, None)
-    train_key, train_des = sift.detectAndCompute(train_img, None)
+    query_key, query_des = detector.detectAndCompute(query_img, None)
+    train_key, train_des = detector.detectAndCompute(train_img, None)
+    log.info('Ключевые точки определены: query ({}), train ({})', len(query_key), len(train_key))
     ### Мэтчинг
-    flann = cv.FlannBasedMatcher(flann_index_params, flann_search_params)
-    matches = flann.knnMatch(query_des, train_des, k=2)
-
+    matcher = cv.FlannBasedMatcher(FLN_INDEX, FLN_SEARCH)
+    log.info('Мэтчер инициализирован: {}', matcher.__class__)
+    matches = matcher.knnMatch(query_des, train_des, k=2)
     ### Определение подходящих мэтчей по проверке Lowe
     good_matches = [m for m, n in matches if m.distance < 0.7 * n.distance]
+    log.info('Результаты мэтчинга: до очистки ({}), после очистки ({})', len(matches), len(good_matches))
 
-    if len(good_matches) < min_match_count: raise Exception(
-        "Недостаточно совпадений найдено - {}/{}".format(len(good_matches), min_match_count)
+    if len(good_matches) < MIN_MATCHES: raise Exception(
+        'Недостаточно совпадений найдено - {}/{}'.format(len(good_matches), MIN_MATCHES)
     )
 
     ### Гомография
     M, mask = cv.findHomography(
         srcPoints=np.float32([query_key[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2),
         dstPoints=np.float32([train_key[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2),
-        method=cv.RANSAC,
-        ransacReprojThreshold=5.0
+        **HOMOGRAPHY
     )
     matches_mask = mask.ravel().tolist()
+    log.info('Поиск гомографии:\n{}', M)
 
     ### Определение крайних точек объекта
     h, w = query_img.shape
@@ -82,57 +88,71 @@ def detector(
         thickness=5, 
         lineType=cv.LINE_AA
     )
+    log.info('Определение координат исходного фрагмента:\n"{}"', [[*x[0]] for x in train_pts])
 
     ### Определение смещения и поворота
     shifted_x = train_pts[-1][0][0] - train_pts[0][0][0]
     shifted_y = train_pts[-1][0][1] - train_pts[0][0][1]
     radius = sqrt(shifted_x**2 + shifted_y**2)
+    angle = round(acos(shifted_x / radius) * 180 / pi, 1) * (1 if shifted_y >= 0 else -1)
+    shifts = {
+        'x1': round(train_pts[0][0][0], 3),
+        'y1': round(train_pts[0][0][1], 3), 
+        'x2': round(train_pts[2][0][0], 3), 
+        'y2': round(train_pts[2][0][1], 3), 
+        'r': angle
+    }
+    log.info('Определение параметров смещения (положительно при повороте по часовой):\n"{}"', shifts,)
 
-    angle = round(acos(shifted_x / radius) * 180 / pi, 1)
-    if round(shifted_y, 1) != 0: angle = angle if shifted_y > 0 else -angle
     ### Вывод итогового результата
-    print([shifted_x, shifted_y, angle,])
+    print([*shifts.values()])
+    log.info('Вывод результата в STDOUT: "{}"', *([*shifts.values()],))
 
-    # ### Отрисовка результата
-    # final_img = cv.drawMatches(
-    #     img1=query_img, 
-    #     keypoints1=query_key, 
-    #     img2=train_img, 
-    #     keypoints2=train_key, 
-    #     matches1to2=good_matches,
-    #     outImg=None,
-    #     matchesMask=matches_mask,
-    #     **{ ### параметры отрисовки
-    #         "matchColor":(0,255,0),
-    #         "singlePointColor":None,
-    #         "flags":2
-    #     }
-    # )
-    # plt.imshow(final_img, 'gray'), plt.show()    
+    ### Отрисовка результата
+    final_img = cv.drawMatches(
+        img1=query_img, 
+        keypoints1=query_key, 
+        img2=train_img, 
+        keypoints2=train_key,
+        matches1to2=good_matches,
+        outImg=None,
+        matchesMask=matches_mask,
+        **{ ### параметры отрисовки
+            "matchColor":(0,255,0),
+            "singlePointColor":None,
+            "flags":2
+        }
+    )
+    plt.imshow(final_img, 'gray'), plt.axis('off')
+    reslt_path = None if not log.LOGS else os.join(log.LOGS, log.JOB+'_reslt.png')
+    log.info('Отрисовка результата' + ('' if not reslt_path else ':\n"{}"'.format(reslt_path)))
+    plt.show() if not log.LOGS else plt.savefig(reslt_path, bbox_inches='tight')
+
+    pass
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     ### Проверка на наличие необходимого/достаточного количества параметров
-    if len(sys.argv[1:]) not in (2, 3,): raise Exception(
-        'Некорректное количество входных параметров: {}.\n'.format(sys.argv[1:]) + \
+    if len(argv[1:]) not in (2, 3,): raise Exception(
+        'Некорректное количество входных параметров: {}.\n'.format(argv[1:]) + \
         'Обязательно наличие пути к образцу, пути к изображению.'
     )
     ### Проверка на корректность указаных путей
-    for path in sys.argv[1:3]:
-        if not os.path.exists(path): raise Exception(
-        'Некорректный путь к файлу: {}.'.format(path)
+    for arg in argv[1:3]:
+        if not os.exists(arg): raise Exception(
+        'Некорректный путь к файлу: {}.'.format(arg)
     )
     ### Проверка на корректность дампа словаря
-    if len(sys.argv[1:]) == 3:
-        try: params = loads(sys.argv[3])
+    if len(argv[1:]) == 3:
+        try: params = loads(argv[3])
         except Exception as exc: raise Exception(
-            'Некорректно задан конфигурационный словарь: {}.\n{}'.format(sys.argv[3], exc)
+            'Некорректно задан конфигурационный словарь: {}.\n{}'.format(argv[3], exc)
         )
     ### Если detect.py вызван корректно
     detector(
-        query_img_path=sys.argv[1], 
-        train_img_path=sys.argv[2], 
+        query_path=argv[1], 
+        train_path=argv[2], 
         ### Нет необходимости в коррекции параметров при первоначальном вызове
         # **({"params": loads(argv[3].replace('\'', '"'))} if len(argv[1:]) == 3 else {})
-        log_to_file=True
+        logfile=True
     )
--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,5 @@
-import os
-import sys
+from os import path as os, makedirs
+from sys import stdout
 from datetime import datetime
 
 class logger(object):
@@ -14,64 +14,59 @@ class logger(object):
         >>> ... func ~ (callable) – выполняемая функция
         >>> return (func ~ callable) – func с набором дополнительных параметров
         """
-
         import logging
         
-        self.JOB_START = datetime.now().replace(microsecond=0)
-        self.JOB_ID = ''.join(x for x in str(self.JOB_START) if x.isdigit())
-        self.JOB_ID = self.JOB_ID[2:8] + '-' + self.JOB_ID[8:]
-        self.LOG_PATH = None
-        os.path.abspath('../logs')
+        self.JOB, self.LOGS = datetime.now().strftime('%y%m%d-%H%M%S'), None
+        
         ### Определение параметров логирования
-        if kwargs.get('log_to_file', False) == True:
-            self.LOG_PATH = os.path.abspath('../logs/{}'.format(self.JOB_START.date()))
-            os.makedirs(self.LOG_PATH, exist_ok=True)
+        if kwargs.get('logfile', True) == True:
+            self.LOGS = os.abspath('../logs/' + str(datetime.now().date()))
+            makedirs(self.LOGS, exist_ok=True)
         logging.basicConfig(
             level=logging.INFO,
             format='%(message)s',
             **(
-                {"stream": sys.stdout} if kwargs.get('log_to_file', False) == False else \
-                {"filename": os.path.join(self.LOG_PATH, '{}.log'.format(self.JOB_ID))}
+                {"stream": stdout} if kwargs.get('logfile', True) == False else \
+                {"filename": os.join(self.LOGS, '{}.log'.format(self.JOB))}
             )
         )
-        self.LOG_PRE, self.LOG_SEP = '[{}] {{{}}} – ', '-'*80
-
         ### Реализация метода логирования класса
-        self.info = lambda message, *args, status='INFO', divide=False, **kwargs: logging.info(
-            ('' if divide == False else self.LOG_SEP + '\n') + \
-            self.LOG_PRE.format(str(datetime.now())[:-7], status) + \
-            message.format(*args, **kwargs).replace('\n', '\n\t') + \
-            ('' if divide == False else '\n' + self.LOG_SEP)
-        )
+        self.info = lambda message, *args, prefix=True, status='INFO', divide=False, **kwargs: \
+            logging.info(
+                ('' if divide == False else '-'*80 + '\n') + \
+                ('' if prefix == False else '[{}] {{{}}} – '.format(str(datetime.now())[:-7], status)) + \
+                (message.format(*args, **kwargs).replace('\n', '\n\t')) + \
+                ('' if divide == False else '\n' + '-'*80)
+            )
         ### Логирование начала выполнения процесса
         self.info(
             'Выполнение задачи ({}), попытка: {}',
-            *(self.JOB_ID, kwargs.get('attempt', 1),),
+            self.JOB,
+            kwargs.get('attempt', 1),
             status='INFO',
             divide=True,
         )
+
         try:
+            status, message = 'SUCCESS', 'выполнена успешно'
             return func(*args, **kwargs, log=self)
         except Exception as e:
-            self.info(str(e))
+            status, message = 'FAILED', 'прервана ошибкой'
+            self.info(str(e), status='ERROR')
         finally:
-            self.info(
-                'Задача ({}) выполнена успешно',
-                *(self.JOB_ID,),
-                status='SUCCESS',
-                divide=True
-            )
-        return
+            self.info('Задача ({}) {}', self.JOB, message, status=status, divide=True)
+        
+        pass
 
     # ### Реализовано в рамках lambda функции self.init() метода
     # def info(self, message:str, *args, status:str='INFO', divide:bool=False, **kwargs) -> None:
     #     """ Логирование сообщения
     #     """
     #     logging.info(
-    #         ('' if divide == False else self.LOG_SEP + '\n') + \
-    #         self.LOG_PRE.format(str(datetime.now())[:-7], status) + \
+    #         ('' if divide == False else self.SEPR + '\n') + \
+    #         self.PREF.format(str(datetime.now())[:-7], status) + \
     #         message.format(*args, **kwargs).replace('\n', '\n\t') + \
-    #         ('' if divide == False else '\n' + self.LOG_SEP)
+    #         ('' if divide == False else '\n' + self.SEPR)
     #     )
 
     def transfer(self, path:str, type:str) -> list:
@@ -79,20 +74,16 @@ class logger(object):
 
         >>> ... path ~ (str) - путь к исходному файлу
         >>> ... type ~ (str) – тип изображения:
-                    'query' – фрагмент
-                    'train' – изображение
+                    = 'query' – фрагмент
+                    = 'train' – изображение
         >>> return (str) – путь к бэкапу изображения
         """
-
-        ### В случае, если вывод осуществляется в sys.stdout, пропустить
-        if not self.LOG_PATH: return path
-
         import shutil
+        
+        if not self.LOGS: return path ### пропустить если вывод осуществляется в stdout
 
-        dest = os.path.join(
-            self.LOG_PATH,
-            '{}_{}.{}'.format(self.JOB_ID, type, path.split('.')[-1])
-        )
+        dest = os.join(self.LOGS, '{}_{}.{}'.format(self.JOB, type, path.split('.')[-1]))
         shutil.copyfile(path, dest, follow_symlinks=True)
-        self.info('Изображение "{}" успешно бэкапировано в \n"{}"', *(path, dest,))
+        self.info('\tИзображение "{}" успешно бэкапировано в "{}"', path, dest, prefix=False)
+        
         return dest
